2c0d9c3ebf20dbbf624fdd9b5e9f6fd997c75ba4
Alex0711 <alex.senger@hotmail.com>
2022-11-16 00:06:18 -0300

    ADD: app.listen()

--- BACK/src/app.js
+++ BACK/src/app.js
@@ -4,16 +4,14 @@ const compression = require("compression");
 const morgan = require("morgan");
 const routerApi = require('./routers/index');
 
-// Routers
-
-// Controllers
-
 // Init our Express app
 const app = express();
 
-routerApi(app);
 // Enable Express app to receive JSON data
 app.use(express.json());
+
+// Routers
+routerApi(app);
 
 app.use(helmet());
 
@@ -22,6 +20,9 @@ app.use(compression());
 app.use(morgan("dev"));
 
 // Define endpoints
+app.listen(3000, () => {
+  console.log(`Servidor funcionando en el puerto 3000`)
+})
 
 // Catch non-existing endpoints
 app.all("*", (req, res) => {
