5dd09480c121ae4c01ccc59a312badd1ac4e99d5
Andres-Developer <andres.burbano.dev@gmail.com>
2022-11-22 01:39:31 -0500

    MOD: adding Auth useEffect

--- FRONT-React/src/components/PrivateRoute.jsx
+++ FRONT-React/src/components/PrivateRoute.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 
 const PrivateRoute = ({ children }) => {
-    const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+    const { user, isAuthenticated, isLoading, loginWithRedirect, getAccessTokenSilently } = useAuth0();
+
+
+    useEffect(() => {
+        const getJWTAuth0Token = async () => {
+            const accessToken = await getAccessTokenSilently({
+                audience: `api-autenticacion-huellitas`
+            });
+            localStorage.setItem("token", accessToken);
+            // console.log("accesToken: ", accessToken);
+        };
+
+        // Solicitar Token cada vez que se autentique
+        if (isAuthenticated) {
+            getJWTAuth0Token();
+        }
+    }, [isAuthenticated, getAccessTokenSilently]);
+
+
     if (isLoading) return <h1>Loading...</h1>;
 
     if (!isAuthenticated) {
