d73ae16e45d393009e3f215baef10fd25025a2e7
Alex0711 <alex.senger@hotmail.com>
2022-11-23 17:56:40 -0300

    UPDATE: sequelizeErrorHandler

--- BACK/src/middlewares/error.handler.js
+++ BACK/src/middlewares/error.handler.js
@@ -1,7 +1,7 @@
 const boom = require("@hapi/boom");
 
 function logErrors(err, req, res, next) {
-  console.log(err)
+  // console.log(err)
   next(err);
 }
 
@@ -9,17 +9,23 @@ function logErrors(err, req, res, next) {
 function sequelizeErrorHandler (err, req, res, next) {
   if (err.message === "Validation error"){
     const { errors } = err
-    throw boom.badRequest(errors[0].message);
+    res.status(400).json({
+      "statusCode": 400,
+      "error": "Bad Request",
+      "message": errors[0].message
+    });
+  } else {
+    next(err);
   }
-  next(err)
 }
 
 function boomErrorHandler(err, req, res, next) {
   if (err.isBoom) {
     const { output } = err;
     res.status(output.statusCode).json(output.payload);
+  } else {
+    next(err);
   }
-  next(err);
 };
 
 function errorHandler(err, req, res, next) {
