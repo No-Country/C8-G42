c6a82f794a9d7aac80317c08c459b993f12d4f3b
Andres-Developer <andres.burbano.dev@gmail.com>
2022-11-26 23:34:45 -0500

    MOD: adding method to connect DB:  dbConnection

--- BACK/utils/database.util.js
+++ BACK/utils/database.util.js
@@ -2,6 +2,8 @@ const { Sequelize } = require("sequelize");
 const { dbConfig } = require("../src/config/config");
 const { setupModels } = require("./../src/persistence/models/initModels");
 
+console.log("dbConfig", dbConfig)
+
 const USER = encodeURIComponent(dbConfig.username);
 const PASSWORD = encodeURIComponent(dbConfig.password);
 const URI = `${dbConfig.dialect}://${USER}:${PASSWORD}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
@@ -10,4 +12,14 @@ const sequelize = new Sequelize(URI);
 
 setupModels(sequelize);
 
-module.exports = sequelize;
+const dbConnection = async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+    }
+  };
+
+// module.exports = sequelize; //original
+module.exports = dbConnection;
