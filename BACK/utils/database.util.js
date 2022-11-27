4fb3686d84157639da58d198c07918f0d9ffbf89
Andres-Developer <andres.burbano.dev@gmail.com>
2022-11-27 00:39:40 -0500

    MOD: revert to sequelize URI

--- BACK/utils/database.util.js
+++ BACK/utils/database.util.js
@@ -2,8 +2,6 @@ const { Sequelize } = require("sequelize");
 const { dbConfig } = require("../src/config/config");
 const { setupModels } = require("./../src/persistence/models/initModels");
 
-console.log("dbConfig", dbConfig)
-
 const USER = encodeURIComponent(dbConfig.username);
 const PASSWORD = encodeURIComponent(dbConfig.password);
 const URI = `${dbConfig.dialect}://${USER}:${PASSWORD}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
@@ -12,14 +10,5 @@ const sequelize = new Sequelize(URI);
 
 setupModels(sequelize);
 
-const dbConnection = async () => {
-    try {
-      await sequelize.authenticate();
-      console.log("Connection has been established successfully.");
-    } catch (error) {
-      console.error("Unable to connect to the database:", error);
-    }
-  };
+module.exports = sequelize;
 
-// module.exports = sequelize; //original
-module.exports = dbConnection;
