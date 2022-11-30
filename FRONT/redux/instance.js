8d3b54bcc9a068cfda78f7d41ed6e07a0628b979
Andres-Developer <andres.burbano.dev@gmail.com>
2022-11-30 01:29:49 -0500

    FIX: ERROR in axios.create HEADERS

--- FRONT/redux/instance.js
+++ FRONT/redux/instance.js
@@ -1,14 +1,12 @@
 import axios from "axios";
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+// const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = 'http://localhost:5000/api/v1/';
 
 const instance = () => {
   return axios.create({
     baseURL: `${API_URL}`,
     headers: {
-      accept: "/",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${
-        token ? token : localStorage.getItem("token")
+      'Authorization': `Bearer ${localStorage.getItem("token")
       }`,
     },
   });
