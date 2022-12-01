b11af6ecffd368529651fafd91d9a5bfeb973c27
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-01 00:23:29 -0500

    MOD: adding comment to code

--- FRONT/redux/instance.js
+++ FRONT/redux/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const API_URL = 'http://localhost:5000/api/v1/';
 
-const token = localStorage.getItem("token");
+// const token = localStorage.getItem("token");
 
 // const headers = {
 //   accept: "/",
