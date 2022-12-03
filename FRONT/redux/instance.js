2c45bb5dc1dbb1f16361587ba4933164834d7621
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-02 23:28:36 -0500

    MOD: reading API_URL from .env

--- FRONT/redux/instance.js
+++ FRONT/redux/instance.js
@@ -1,21 +1,5 @@
 import axios from "axios";
-// const API_URL = process.env.NEXT_PUBLIC_API_URL;
-// const API_URL = 'http://localhost:5000/api/v1/';
-const API_URL = 'https://backend.huellitas.ar/';
-
-// const token = localStorage.getItem("token");
-
-// const headers = {
-//   accept: "/",
-//   "Content-Type": "application/json",
-//   // Authorization: `Bearer ${
-//   //   token ? token : localStorage.getItem("token")
-//   // }`,
-// };
-
-// if (token) {
-//   headers.Authorization = `Bearer ${token}`;
-// }
+const API_URL = process.env.NEXT_PUBLIC_API_URL;  // Reading .env:   'http://localhost:5000/api/v1/' or 'https://backend.huellitas.ar/'
 
 const instance = () => {
   return axios.create({
