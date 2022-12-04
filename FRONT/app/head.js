31871b61f63be7bfa087c38ff64c2d63a6a82485
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-04 14:46:24 -0500

    MOD: title in head

--- FRONT/app/head.js
+++ FRONT/app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Huellitas | Adopción de Mascotas </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
     </>
