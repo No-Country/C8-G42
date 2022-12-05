cbe7db6052afcc9ed8c5ea5ddc55c5d2a3574a7c
Alex0711 <alex.senger@hotmail.com>
2022-12-04 21:31:10 -0300

    messenger can update messages with websockets

--- FRONT/components/Messenger/Messenger.jsx
+++ FRONT/components/Messenger/Messenger.jsx
@@ -1,24 +1,31 @@
-import { useDisclosure, Button, Drawer } from "@chakra-ui/react";
+import {
+  useDisclosure,
+  Button,
+  Drawer,
+} from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchShelter, fetchShelters } from "../../redux/slices/sheltersSlice";
 import ChatsContainer from "./chats/ChatsContainer";
 import socket from "../../utils/socket";
+import { addMessage, fetchChat } from "../../redux/slices/messangerSlice";
 
 const placement = "right";
 
 const Messenger = () => {
   const user = useSelector((state) => state.user.user);
+  const chats = useSelector((state) => state.messenger.chats);
+  console.log({ chats });
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const dispatch = useDispatch();
   useEffect(() => {
-    if (user?.role ==="user") {
+    if (user?.role === "user") {
       dispatch(fetchShelters({ limit: 10, offset: 0 }));
     }
-    if (user?.role === "shelterOwner"){
-      dispatch(fetchShelter({shelterId: user.shelter.id}))
+    if (user?.role === "shelterOwner") {
+      dispatch(fetchShelter({ shelterId: user.shelter.id }));
     }
     if (user?.role) {
       socket.auth = {
@@ -29,9 +36,21 @@ const Messenger = () => {
     }
   }, [user]);
 
-  socket.on("message", () => {
-    
-  })
+  socket.on("message", (message) => {
+    const chatId = `${message.shelterId}${message.userId}`;
+    if (chats[chatId]) {
+      dispatch(addMessage);
+    } else {
+      dispatch(
+        fetchChat({
+          userId: message.userId,
+          shelterId: message.shelterId,
+          limit: 40,
+          offset: 0,
+        })
+      );
+    }
+  });
 
   if (user) {
     return (
