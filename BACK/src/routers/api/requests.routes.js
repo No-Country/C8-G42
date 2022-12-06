42e2daf959959686e72c00e1f72626323d415c5f
Alex0711 <alex.senger@hotmail.com>
2022-12-06 16:55:21 -0300

    ADD: request schema, route & controller

---
+++ BACK/src/routers/api/requests.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const requestController = require("../../controllers/requests.controller")
+const schemaValidator = require("../../middlewares/schema.validator");
+const { createRequestSchema } = require("../../schemas/request.schema");
+
+router.post(
+  "/",
+  schemaValidator(createRequestSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const requestData = req.body;
+      const newRequest = await requestController.create(requestData);
+      console.log({newRequest})
+      return res.status(201).json(newRequest);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
