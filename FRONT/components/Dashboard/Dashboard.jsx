ab7c6ff47644baea59c1321967deabacf36ea01c
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-06 01:45:21 -0500

    MOD: Adding spiner while load user info

--- FRONT/components/Dashboard/Dashboard.jsx
+++ FRONT/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Spinner, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import PetsAdoption from './PetsAdoption';
@@ -9,11 +9,25 @@ const Dashboard = () => {
   const user = useSelector((state) => state.user.user);
 
   return (
-    <Flex>
-      {console.log("user: ", user)}
-      <SideBar />
-      <PetsAdoption />
-    </Flex>
+    <>
+      {!user && <Flex
+        justifyContent="center"
+        alignItems="center"
+        height="50vh"
+        direction="column"
+        gap="6"
+      >
+        <Text>Cargando info usuario ...</Text>
+        <Spinner color="blue.500" />
+      </Flex>}
+      {
+        user &&
+        <Flex>
+          {console.log("user: ", user)}
+          <SideBar user={user} />
+          <PetsAdoption />
+        </Flex>}
+    </>
   );
 };
 
