80314474dd74d22dee27026ee065888a63c22020
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-06 00:18:16 -0500

    MOD: add console.log (user)

--- FRONT/components/Dashboard/Dashboard.jsx
+++ FRONT/components/Dashboard/Dashboard.jsx
@@ -1,19 +1,20 @@
-'use client'
-import {Flex } from '@chakra-ui/react'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import PetsAdoption from './PetsAdoption'
-import SideBar from './SideBar'
+'use client';
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import PetsAdoption from './PetsAdoption';
+import SideBar from './SideBar';
 
 const Dashboard = () => {
   const user = useSelector((state) => state.user.user);
-  
+
   return (
     <Flex>
-        <SideBar/>
-        <PetsAdoption/>
+      {console.log("user: ", user)}
+      <SideBar />
+      <PetsAdoption />
     </Flex>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
