95ae166ed9313ecaa3c353bc099c1f5bbb6d22e8
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-06 01:44:42 -0500

    MOD: protecting /dahboard page

--- FRONT/app/dashboard/page.jsx
+++ FRONT/app/dashboard/page.jsx
@@ -1,13 +1,30 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Dashboard from '../../components/Dashboard/Dashboard';
+import { useUser } from "@auth0/nextjs-auth0";
+import { Button, Flex, Link, Text } from '@chakra-ui/react';
 
 
 const Dash = () => {
+  const { user, isLoading } = useUser();
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <>
+      {!user &&
+
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          height="50vh"
+          direction="column"
+          gap="6"
+        >
+          <Text>Esta es una ruta protegida, por favor inicia sesión.</Text>
+          <Button><Link href="/api/auth/login">Login</Link></Button>
+        </Flex>
+
+      }
+      {user && <Dashboard />}
+    </>
   );
 };
 
