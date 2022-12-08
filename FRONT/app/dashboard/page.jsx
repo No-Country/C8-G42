e6b113ad4e1ac749c40c84f07a425bc7242cb057
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-08 10:13:22 -0500

    MOD: Adding loading spinner

--- FRONT/app/dashboard/page.jsx
+++ FRONT/app/dashboard/page.jsx
@@ -2,16 +2,32 @@
 import React, { useEffect } from 'react';
 import Dashboard from '../../components/Dashboard/Dashboard';
 import { useUser } from "@auth0/nextjs-auth0";
-import { Button, Flex, Link, Text } from '@chakra-ui/react';
+import { Button, Flex, Link, Spinner, Text } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
 
 
 const Page = () => {
   const user = useSelector((state) => state.user.user);
+  const { user: userAuth0, isLoading: loading } = useUser();
+
+  if (loading) {
+    return (
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        height="50vh"
+        direction="column"
+        gap="6"
+      >
+        <Text>Cargando dashboard...</Text>
+        <Spinner color="blue.500" />
+      </Flex>
+    );
+  };
+
   return (
     <>
-      {!user &&
-
+      {!user && !userAuth0 &&
         <Flex
           justifyContent="center"
           alignItems="center"
