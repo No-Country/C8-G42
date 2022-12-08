b5fee6194a5f06da79e4a5c058d8459595624eac
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-08 10:16:13 -0500

    FIX: Shelter undefined id

--- FRONT/components/Messenger/Messenger.jsx
+++ FRONT/components/Messenger/Messenger.jsx
@@ -23,7 +23,7 @@ const Messenger = () => {
       dispatch(fetchShelters({ limit: 10, offset: 0 }));
     }
     if (user?.role === "shelterOwner") {
-      dispatch(fetchShelter({ shelterId: user.shelter.id }));
+      dispatch(fetchShelter({ shelterId: user?.shelter?.id }));
     }
     if (user?.role) {
       socket.auth = {
