b9ddb1fdd3d5e30bdf9fd08a3f663ebc07c58c3d
Andres-Developer <andres.burbano.dev@gmail.com>
2022-12-08 12:42:59 -0500

    MOD: ignore EsLint

--- FRONT/next.config.js
+++ FRONT/next.config.js
@@ -12,6 +12,11 @@ const nextConfig = {
     appDir: true,
   },
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
